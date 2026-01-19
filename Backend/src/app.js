f6a15f5e12680f2b049c306894fb97b92d94662a
Vivek Rathore <vivekrathore1228@gmail.com>
2026-01-19 22:07:44 +0530

    fix: update CORS preflight handling to use regex for better matching

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -50,7 +50,8 @@ app.use(
 );
 
 // ✅ VERY IMPORTANT: handle preflight properly
-app.options("*", cors());
+app.options(/.*/, cors());
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
